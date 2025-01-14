af5565d3e17adbcc77bd333f5508c79e95bd809d
EvanReact <evanhill1989@gmail.com>
2025-01-14 10:11:25 -0500

    uploadthing fix

--- app/api/uploadthing/route.ts
+++ app/api/uploadthing/route.ts
@@ -3,6 +3,8 @@ import { createRouteHandler } from "uploadthing/next";
 import { ourFileRouter } from "./core";
 
 // Export routes for Next App Router
+console.log("Uploadthing route initialized");
+
 export const { GET, POST } = createRouteHandler({
   router: ourFileRouter,
 
