00f7803105e2795e5bfa3d064959af34542e7071
Evan Hill <evanhill1989@gmail.com>
2025-01-20 15:16:30 -0500

    AuthProvider

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 // import { ThemeProvider } from "../components/dashboard/ThemeProvider";
 import { Toaster } from "@/components/ui/toaster";
+import { AuthProvider } from "@/components/dashboard/AuthProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -35,8 +36,11 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         > */}
-        {children}
-        <Toaster />
+        <AuthProvider>
+          {children}
+
+          <Toaster />
+        </AuthProvider>
         {/* </ThemeProvider> */}
       </body>
     </html>
