b813dd89613f2977dad1826e123e99349e808cdc
Peter Phanouvong <peterphanouvong@gmail.com>
2025-01-23 15:51:13 +1100

    fix: rm prisma

--- app/api/auth/creation/route.ts
+++ app/api/auth/creation/route.ts
@@ -1,5 +1,4 @@
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
-import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
 export async function GET() {
@@ -9,24 +8,24 @@ export async function GET() {
     throw new Error("User not found");
   }
 
-  let dbUser = await prisma.user.findUnique({
-    where: {
-      id: user.id,
-    },
-  });
+  // let dbUser = await prisma.user.findUnique({
+  //   where: {
+  //     id: user.id,
+  //   },
+  // });
 
-  if (!dbUser) {
-    dbUser = await prisma.user.create({
-      data: {
-        id: user.id,
-        firstName: user.given_name ?? "",
-        lastName: user.family_name ?? "",
-        email: user.email ?? "",
-        profileImage:
-          user.picture ?? `https://avatar.vercel.sh/${user.given_name}`,
-      },
-    });
-  }
+  // if (!dbUser) {
+  //   dbUser = await prisma.user.create({
+  //     data: {
+  //       id: user.id,
+  //       firstName: user.given_name ?? "",
+  //       lastName: user.family_name ?? "",
+  //       email: user.email ?? "",
+  //       profileImage:
+  //         user.picture ?? `https://avatar.vercel.sh/${user.given_name}`,
+  //     },
+  //   });
+  // }
 
   return NextResponse.redirect(
     process.env.NODE_ENV === "production"
